b563801919fed9712afb2ea67de15defa84eb328
Nahum Mejia <162595000+NahumMejia@users.noreply.github.com>
2025-09-24 13:56:58 -0600

    Section 3/video 19 (#7)
    * Añadido: ruta de lista de roles y creación de componentes
    
    * Modificado componente de list-roles así como creación de funciones para el click
    
    * Añadido: Crear roles y asignar permisos
    
    * Creado servicio de roles, modificado el typescript de creacion de roles

--- src/app/modules/roles/create-roles/create-roles.component.ts
+++ src/app/modules/roles/create-roles/create-roles.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { SIDEBAR } from 'src/app/config/config';
+import { RolesService } from '../service/roles.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-create-roles',
@@ -11,14 +13,41 @@ export class CreateRolesComponent {
   name: String = '';
   SIDEBAR: any = SIDEBAR;
   isLoading : any;
-  constructor(public modal:NgbActiveModal){
+  permisions : any = [];
+
+  constructor(public modal:NgbActiveModal, public roleService: RolesService, public toast: ToastrService,){
      
   }
   ngOnInit():void {
 
   }
+  addPermission(permiso:string){
+    let INDEX = this.permisions.findIndex((perm:string)=> perm == permiso);
+    if (INDEX != -1){
+      this.permisions.splice(INDEX,1);
+    }else{
+      this.permisions.push(permiso);
+    }
+    console.log(this.permisions);
+  }
+
 
   store(){
+    if(!this.name){
+    this.toast.error("Validacion", "El nombre es requerido");
+      return false;
+  }
+  if(this.permisions.length == 0){
+    this.toast.error("Validacion", "Necesitas seleccionar un permiso");
+    return false;
+  }
+    let data = {
+      name: this.name,
+      permisions: this.permisions,
+    }
+    this.roleService.registerRole(data).subscribe((resp:any)=> {
+        console.log(resp)
+    })
 
   }
 }
