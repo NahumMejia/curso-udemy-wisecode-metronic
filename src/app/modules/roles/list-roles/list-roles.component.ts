cb739aa467e9b865624cb54b73ec0eab532d2871
Nahum Mejia <162595000+NahumMejia@users.noreply.github.com>
2025-09-24 14:44:35 -0600

    Section 3/video 20 (#8)
    * Añadido: ruta de lista de roles y creación de componentes
    
    * Modificado componente de list-roles así como creación de funciones para el click
    
    * Añadido: Crear roles y asignar permisos
    
    * Creado servicio de roles, modificado el typescript de creacion de roles
    
    * Añadido el listado de roles en la tabla

--- src/app/modules/roles/list-roles/list-roles.component.ts
+++ src/app/modules/roles/list-roles/list-roles.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateRolesComponent } from '../create-roles/create-roles.component';
+import { RolesService } from '../service/roles.service';
 
 @Component({
   selector: 'app-list-roles',
@@ -8,11 +9,29 @@ import { CreateRolesComponent } from '../create-roles/create-roles.component';
   styleUrls: ['./list-roles.component.scss']
 })
 export class ListRolesComponent {
-  constructor(public modalService: NgbModal,){
+  search:string = '';
+  ROLES: any = [];
+  isLoading$:any;
+  totalPages:number = 0;
+  currentPage:number = 0;
+  constructor(public modalService: NgbModal, public rolesService:RolesService,){
   }
 
   ngOnInit():void {
-    
+    this.isLoading$ = this.rolesService.isLoading$;
+    this.listRoles();
+  }
+
+  listRoles(page = 1){
+    this.rolesService.listRoles(page,this.search).subscribe((resp:any)=>{
+      console.log(resp);
+      this.ROLES = resp.roles;
+      this.totalPages = resp.total;
+      this.currentPage = page;
+    })
+  }
+  loadPage($event:any){
+    this.listRoles($event);
   }
 
   createRol(){
