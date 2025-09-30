53612b50133d4a16842a14adf3284a0892e8e635
K1535 <167162183+K1535@users.noreply.github.com>
2025-09-30 15:15:19 -0600

    service list sucursal, create, edit, delete

---
+++ src/app/modules/configuration/sucursales/create-sucursal/create-sucursal.component.ts
@@ -0,0 +1,69 @@
+import { Component, EventEmitter } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { SIDEBAR } from 'src/app/config/config';
+import { RolesService } from 'src/app/modules/roles/service/roles.service';
+
+@Component({
+  selector: 'app-create-sucursal',
+  templateUrl: './create-sucursal.component.html',
+  styleUrls: ['./create-sucursal.component.scss']
+})
+export class CreateSucursalComponent {
+
+
+  @Output() RoleC: EventEmitter<any> = new EventEmitter();
+    name: String = '';
+    SIDEBAR: any = SIDEBAR;
+    isLoading : any;
+    permisions : any = [];
+  
+    constructor(public modal:NgbActiveModal, public roleService: RolesService, public toast: ToastrService,){
+       
+    }
+    ngOnInit():void {
+  
+    }
+    addPermission(permiso:string){
+      let INDEX = this.permisions.findIndex((perm:string)=> perm == permiso);
+      if (INDEX != -1){
+        this.permisions.splice(INDEX,1);
+      }else{
+        this.permisions.push(permiso);
+      }
+      console.log(this.permisions);
+    }
+  
+  
+    store(){
+      if(!this.name){
+      this.toast.error("Validacion", "El nombre es requerido");
+        return false;
+    }
+    if(this.permisions.length == 0){
+      this.toast.error("Validacion", "Necesitas seleccionar un permiso");
+      return false;
+    }
+      let data = {
+        name: this.name,
+        permisions: this.permisions,
+      }
+      this.roleService.registerRole(data).subscribe((resp:any)=> {
+          console.log(resp)
+          if(resp.message == 403){
+            this.toast.error('Validacion',resp.message_text);
+          }else{
+            this.toast.success("Éxito","El rol se registró correctamente");
+            this.RoleC.emit(resp.role);
+            this.modal.close(); 
+          }
+      })
+  
+    }
+  }
+  
+
+function Output(): (target: CreateSucursalComponent, propertyKey: "RoleC") => void {
+  throw new Error('Function not implemented.');
+}
+
---
+++ src/app/modules/configuration/sucursales/sucursales-routing.module.ts
@@ -0,0 +1,24 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SucursalesComponent } from './sucursales.component';
+import { ListSucursalComponent } from './list-sucursal/list-sucursal.component';
+
+const routes: Routes = [
+
+  {
+    path:"",
+    component: SucursalesComponent,
+    children:[
+      {
+        path: "list",
+        component: ListSucursalComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SucursalesRoutingModule { }
