566b32924fd3e3282c0241bd2f395da1616043de
K1535 <167162183+K1535@users.noreply.github.com>
2025-09-30 17:31:22 -0600

    deleted y edit sucursal

--- src/app/modules/configuration/sucursales/delete-sucursal/delete-sucursal.component.ts
+++ src/app/modules/configuration/sucursales/delete-sucursal/delete-sucursal.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SucursalService } from '../service/sucursal.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-delete-sucursal',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-sucursal.component.scss']
 })
 export class DeleteSucursalComponent {
+
+  @Output() SucursalD: EventEmitter<any> = new EventEmitter();
+  @Input()  SUCURSAL_SELECTED:any;
+
+  isLoading:any;
+  constructor(
+    public modal: NgbActiveModal,
+    public sucursalesService: SucursalService,
+    public toast: ToastrService,
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  delete(){
+    
+    this.sucursalesService.deleteSucursal(this.SUCURSAL_SELECTED.id).subscribe((resp:any) => {
+      console.log(resp);
+      if(resp.message == 403){
+        this.toast.error("Validación",resp.message_text);
+      }else{
+        this.toast.success("Exito","La sucursal se elimino correctamente");
+        this.SucursalD.emit(resp.message);
+        this.modal.close();
+      }
+    })
+  }
 
 }
--- src/app/modules/configuration/sucursales/list-sucursal/list-sucursal.component.ts
+++ src/app/modules/configuration/sucursales/list-sucursal/list-sucursal.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SucursalService } from '../service/sucursal.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateSucursalComponent } from '../create-sucursal/create-sucursal.component';
 import { EditSucursalComponent } from '../edit-sucursal/edit-sucursal.component';
 import { DeleteSucursalComponent } from '../delete-sucursal/delete-sucursal.component';
@@ -13,59 +13,69 @@ import { DeleteSucursalComponent } from '../delete-sucursal/delete-sucursal.comp
 export class ListSucursalComponent {
 
   search:string = '';
-    SUCURSALES: any = [];
-    isLoading$:any;
-    totalPages:number = 0;
-    currentPage:number = 0;
-    constructor(
-      public modalService: NgbModal, 
-      public sucursalService:SucursalService,
-    ){
-      
-    }
-  
-    // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
-    ngOnInit():void {
-      this.isLoading$ = this.sucursalService.isLoading$;
-      this.listSucursales();
-    }
-  
-    listSucursales(page = 1){
-      this.sucursalService.listSucursales(page,this.search).subscribe((resp:any)=>{
-        console.log(resp);
-        this.SUCURSALES = resp.sucursales;
-        this.totalPages = resp.total;
-        this.currentPage = page;
-      })
-    }
-    loadPage($event:any){
-      this.listSucursales($event);
-    }
-  
-    createSucursal(){
-      const modalRef = this.modalService.open(CreateSucursalComponent, {centered:true, size:'md'});
-      modalRef.componentInstance.SucursalC.subscribe((sucursal:any)=>{
-        this.SUCURSALES.unshift(sucursal);
-      })
-    }
-    editSucursal(ROL:any){
-      const modalRef = this.modalService.open(EditSucursalComponent, {centered:true, size:'md'});
-      modalRef.componentInstance.ROLE_SELECTED = ROL;
-      modalRef.componentInstance.RoleE.subscribe((role:any)=>{
-        let INDEX = this.SUCURSALES.findIndex((rol:any) => rol.id == ROL.id);
-        if(INDEX != -1){
-          this.SUCURSALES[INDEX] = role;
-        }
-      })
-    }
-    deleteSucursal(ROL:any){
-      const modalRef = this.modalService.open(DeleteSucursalComponent, {centered:true, size:'md'});
-      modalRef.componentInstance.ROLE_SELECTED = ROL;
-      modalRef.componentInstance.RoleD.subscribe((role:any)=>{
-        let INDEX = this.SUCURSALES.findIndex((rol:any) => rol.id == ROL.id);
-        if(INDEX != -1){
-          this.SUCURSALES.splice(INDEX,1);
-        }
-      })
-    }
+  SUCURSALES:any = [];
+  isLoading$:any;
+
+  totalPages:number = 0;
+  currentPage:number = 1;
+  constructor(
+    public modalService: NgbModal,
+    public sucursalService: SucursalService,
+  ) {
+    
   }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.isLoading$ = this.sucursalService.isLoading$;
+    this.listSucursales();
+  }
+
+  listSucursales(page = 1){
+    this.sucursalService.listSucursales(page,this.search).subscribe((resp:any) => {
+      console.log(resp);
+      this.SUCURSALES = resp.sucursales;
+      this.totalPages = resp.total;
+      this.currentPage = page;
+    })
+  }
+
+  loadPage($event:any){
+    this.listSucursales($event);
+  }
+
+  createSucursal(){
+    const modalRef = this.modalService.open(CreateSucursalComponent,{centered:true, size: 'md'});
+
+    modalRef.componentInstance.SucursalC.subscribe((sucursal:any) => {
+      this.SUCURSALES.unshift(sucursal);
+    })
+  }
+
+  editSucursal(SUCURSAL:any){
+    const modalRef = this.modalService.open(EditSucursalComponent,{centered:true, size: 'md'});
+    modalRef.componentInstance.SUCURSAL_SELECTED = SUCURSAL;
+
+    modalRef.componentInstance.SucursalE.subscribe((sucursal:any) => {
+      let INDEX = this.SUCURSALES.findIndex((sucurs:any) => sucurs.id == SUCURSAL.id);
+      if(INDEX != -1){
+        this.SUCURSALES[INDEX] = sucursal;
+      }
+    })
+  }
+
+  deleteSurcusal(SUCURSAL:any){
+    const modalRef = this.modalService.open(DeleteSucursalComponent,{centered:true, size: 'md'});
+    modalRef.componentInstance.SUCURSAL_SELECTED = SUCURSAL;
+
+    modalRef.componentInstance.SucursalD.subscribe((sucursal:any) => {
+      let INDEX = this.SUCURSALES.findIndex((sucurs:any) => sucurs.id == SUCURSAL.id);
+      if(INDEX != -1){
+        this.SUCURSALES.splice(INDEX,1);
+      }
+      // this.ROLES.unshift(role);
+    })
+  }
+
+}
